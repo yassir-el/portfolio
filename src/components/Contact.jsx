387a930cdcd8967ed3333b152934469bff896cb6
yassir-el <elyassirzagora@gmail.com>
2024-09-26 08:58:11 +0100

    add

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,24 @@ import Lottie from 'react-lottie';
 import animationData from '../assets/animation/mail-me.json'
 
 import { LuMessageSquare } from 'react-icons/lu'
-import { delay } from 'framer-motion';
+
+import { motion } from 'framer-motion'
+
+
+const variants = {
+    initial: {
+        y: 500,
+        opacity: 0,
+      },
+      animate: {
+        y: 0,
+        opacity: 1,
+        transition: {
+          duration: 0.5,
+          staggerChildren: 0.1,
+        },
+      },
+}
 
 const Contact = () => {
     return (
@@ -20,9 +37,13 @@ const Contact = () => {
         //     </form>
         //         <button type='submit' className='block w-full p-2 my-2 bg-[#854CE6] px-5 text-white rounded-md'>Submit</button>
         // </div>
-        <>
+        <motion.div variants={variants}
+            initial='initial'
+            whileInView="animate"
+            >
+            {/* <h3 className='gap-7 flex justify-center'> */}
 
-            <div className='flex flex-col gap-7 text-4xl pb-4  pt-20'>
+            <motion.div variants={variants} className='flex flex-col gap-7 text-4xl pb-4  pt-20'>
 
                 <h3 className='gap-7 flex justify-center'>
                     <LuMessageSquare className='' />
@@ -31,26 +52,26 @@ const Contact = () => {
                 <p className='text-2xl text-gray-400 text-center'>
                     Submit the form below to get in touch with me.
                 </p>
-            </div>
-            <div id='contact' className=' mt-8 pb-16 flex items-center flex-row gap-14 justify-center'>
-                <div>
+            </motion.div>
+            <motion.div variants={variants} id='contact' className=' mt-8 pb-16 flex items-center flex-row gap-14 justify-center'>
+                <motion.div variants={variants}>
 
-                    <div className='w-96'>
-                        <form action="https://getform.io/f/qbloonza" method='POST' className='flex flex-col justify-center gap-5'>
+                    <motion.div variants={variants} className='w-96'>
+                        <form action="https://getform.io.test/f/qbloonza" method='POST' className='flex flex-col justify-center gap-5'>
 
-                            <input type="text" name='name' placeholder='Enter your name' className='bg-transparent border py-4 px-4 ' />
-                            <input type="email" name='email' placeholder='Enter your email' className='bg-transparent border py-4 px-4 ' />
-                            <textarea placeholder='Enter your message' name="message" id="" rows={5} className='bg-transparent border py-2 px-4'>
+                            <input required type="text" name='name' placeholder='Enter your name' className='bg-transparent border py-4 px-4 ' />
+                            <input required type="email" name='email' placeholder='Enter your email' className='bg-transparent border py-4 px-4 ' />
+                            <textarea required placeholder='Enter your message' name="message" id="" rows={5} className='bg-transparent border py-2 px-4'>
 
                             </textarea>
                             <button className='w-fit border px-6 py-3 m-auto'>
                                 Submit
                             </button>
                         </form>
-                    </div>
+                    </motion.div>
 
-                </div>
-                <div>
+                </motion.div>
+                <motion.div variants={variants}>
                     <Lottie
                         options={
                             {
@@ -66,10 +87,10 @@ const Contact = () => {
                         height={400}
                         width={400}
                     />
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
 
-        </>
+        </motion.div>
     )
 }
 
